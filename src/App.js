17e86e7617304763c3b04d0ae3ac455c1552c6c3
Tvamsikrishna369 <nanivamsi369@gmail.com>
2024-10-25 19:16:48 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styles from './styles.css';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(username === "user" && password === 'password'){
+      setError("");
+      setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.form}>
+      <h1>Login Page</h1>
+      {isSubmitted ? (
+        <div>
+          <p className={styles.formLabel}>Wlecome, {username}!</p>
+        </div>
+      ) : (
+        <form className={styles.formContainer} onSubmit={handleSubmit}>
+          {error && <p className={styles.error} >{error}</p>}
+          <div>
+              <label className={styles.formLabel} htmlFor='username'>
+                Username : 
+              </label>
+              <input
+                className={styles.formInput}
+                type='text'
+                id='username'
+                placeholder='username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+          </div>
+          <div>
+            <label className={styles.formLabel} htmlFor='password'>
+              password : 
+            </label>
+            <input
+              className={styles.formInput}
+              type='text'
+              id='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <div>
+            <button className={styles.formButton} type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
-}
+};
 
 export default App;
