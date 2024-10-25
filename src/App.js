52323436ddee273fc8791ca6c0bc8d408be00a53
Tvamsikrishna369 <nanivamsi369@gmail.com>
2024-10-25 19:50:24 +0530

    5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, {useState} from 'react';
-import styles from './styles.css';
+import React, { useState } from "react";
+import styles from "./styles.css";
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(username === "user" && password === 'password'){
+
+    if (username === "user" && password === "password") {
       setError("");
       setIsSubmitted(true);
     } else {
@@ -17,39 +18,40 @@ const App = () => {
       setIsSubmitted(false);
     }
   };
+
   return (
     <div className={styles.form}>
       <h1>Login Page</h1>
       {isSubmitted ? (
         <div>
-          <p className={styles.formLabel}>Wlecome, {username}!</p>
+          <p className={styles.formLabel}>Welcome, {username}!</p>
         </div>
       ) : (
         <form className={styles.formContainer} onSubmit={handleSubmit}>
-          {error && <p className={styles.error} >{error}</p>}
+          {error && <p className={styles.error}>{error}</p>}
           <div>
-              <label className={styles.formLabel} htmlFor='username'>
-                Username : 
-              </label>
-              <input
-                className={styles.formInput}
-                type='text'
-                id='username'
-                placeholder='username'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-          </div>
-          <div>
-            <label className={styles.formLabel} htmlFor='password'>
-              password : 
+            <label className={styles.formLabel} htmlFor="username">
+              Username :
             </label>
             <input
               className={styles.formInput}
-              type='text'
-              id='password'
-              placeholder='password'
+              type="text"
+              id="username"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className={styles.formLabel} htmlFor="password">
+              Password :
+            </label>
+            <input
+              className={styles.formInput}
+              type="text"
+              id="password"
+              placeholder="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
@@ -57,7 +59,7 @@ const App = () => {
           </div>
 
           <div>
-            <button className={styles.formButton} type='submit'>
+            <button className={styles.formButton} type="submit">
               Submit
             </button>
           </div>
